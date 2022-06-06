56766fd0ba46e46862e65173fddfdf82bfc01fc1
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-06-06 17:11:30 +0000

    salvo

---
+++ numeros.java
@@ -0,0 +1,10 @@
+public class numeros
+{
+    public static void main(String[] args) 
+    {
+        for(int num=38; num <= 1000; num++)
+        {
+            System.out.println(num);
+        }
+    }
+}
